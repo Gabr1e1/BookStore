4b9f803eb42b22c1ee6c82a10c0c12b6adfea96b
Gabriel <zhangzhicheng1@126.com>
2018-12-01 01:34:29 +0800

    Account class with all functionality
    Not even compiled yet

---
+++ account.cpp
@@ -0,0 +1,153 @@
+#include "account.h"
+
+User::User(const int _level, const std::string &_id, 
+	const std::string &_name, const std::string &_password) : 
+	level(_level), userId(_id), name(_name), password(_password), deleted(false)
+{
+	/* Empty */
+}
+
+std::string User::printToString()
+{
+	char *t = new char[UserLen];
+	sprintf(t, "%-1d", level);
+	sprintf(t + NumLen, "%-30s", userId);
+	sprintf(t + NumLen + StringLen, "%-30s", name);
+	sprintf(t + NumLen + 2 * StringLen, "%-30s", password);
+	sprintf(t + NumLen + 3 * StringLen, "%-1d", deleted);
+	std::string ret = t;
+	delete[] t;
+	return ret;
+}
+
+AccountSystem::AccountSystem(const std::string &file) : curLevel(0), size(0)
+{
+	dataIO.open(file, std::ios::out|std::ios::app);
+	User root(7, "root", "root", "sjtu");
+	printToBack(root.printToString());
+}
+
+AccountSystem::~AccountSystem()
+{
+	dataIO.close();
+}
+
+void AccountSystem::printToBack(const std::string &str)
+{
+	auto *t = str.c_str();
+	dataIO.write(t, str.length());
+	delete[] t;
+}
+
+std::string AccountSystem::read(int address, int len)
+{
+	char *t = new char[len];
+	dataIO.seekg(address);
+	dataIO.write(t, len);
+	std::string ret = t;
+	delete[] t;
+	return ret;
+}
+
+void AccountSystem::write(int address, const std::string &str)
+{
+	dataIO.seekg(address);
+	dataIO << str;
+}
+
+std::string AccountSystem::readUserId(int address)
+{
+	return read(address + NumLen, StringLen);
+}
+
+std::string AccountSystem::readPassword(int address)
+{
+	return read(address + NumLen + 2 * StringLen, StringLen);
+}
+
+int AccountSystem::readLevel(int address)
+{
+	return read(address, NumLen)[0] - '0';
+}
+
+void AccountSystem::add(int level, const std::string &userId,
+	const std::string &password, const std::string &name)
+{
+	if (!(curLevel >= 3)) throw("Invalid");
+	if (level >= curLevel) throw("Invalid");
+	User cur(level, userId, name, password);
+	printToBack(cur.printToString());
+	size++;
+}
+
+void AccountSystem::erase(const std::string &userId)
+{
+	if (!(curLevel >= 7)) throw("Invalid");
+	int curAddress = 0;
+	dataIO.seekg(curAddress);
+	for (int i = 1; i <= size; i++)
+	{
+		std::string curUserId = readUserId(curAddress);
+		if (curUserId == userId)
+		{
+			dataIO.seekg(curAddress + UserLen - 1);
+			dataIO << '0';
+			return;
+		}
+		curAddress += UserLen;
+	}
+	throw("Invalid");
+}
+
+void AccountSystem::changePassword(const std::string &userId, const std::string &newPassword,
+	const std::string &oldPassword = "")
+{
+	if (!(curLevel >= 1)) throw("Invalid");
+	int curAddress = 0;
+	dataIO.seekg(curAddress);
+	for (int i = 1; i <= size; i++)
+	{
+		std::string curUserId = readUserId(curAddress);
+		if (curUserId == userId)
+		{
+			std::string curPassword = readPassword(curAddress);
+			if (curPassword == oldPassword || curLevel == 7)
+			{
+				write(curAddress + NumLen + 2 * StringLen, newPassword);
+				return;
+			}
+			else throw("Inavlid");
+		}
+		curAddress += UserLen;
+	}
+	throw("Invalid");
+}
+
+void AccountSystem::login(const std::string &userId, const std::string &password)
+{
+	int curAddress = 0;
+	dataIO.seekg(curAddress);
+	for (int i = 1; i <= size; i++)
+	{
+		std::string curUserId = readUserId(curAddress);
+		if (curUserId == userId)
+		{
+			std::string curPassword = readPassword(curAddress);
+			int level = readLevel(curAddress);
+			if (curPassword == password || curLevel > level)
+			{
+				curUserId = userId;
+				curLevel = level;
+				return;
+			}
+			else throw("Inavlid");
+		}
+	}
+	throw("Invalid");
+}
+
+void AccountSystem::logout()
+{
+	curLevel = 0;
+}
+
---
+++ account.h
@@ -0,0 +1,61 @@
+#ifndef Account_H
+#define Account_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+static const int NumLen = 1;
+static const int StringLen = 30;
+static const int UserLen = NumLen * 2 + 3 * StringLen;
+
+class User
+{
+private:
+	int level;
+	std::string userId; //aligned to 30
+	std::string name; //aligned to 30
+	std::string password; //algiend to 30
+	bool deleted;
+
+public:
+	User(int _level, const std::string &_id, 
+		const std::string &_name, const std::string &_password);
+
+public:
+	std::string printToString();
+};
+
+class AccountSystem
+{
+private:
+	std::fstream dataIO;
+	int curLevel;
+	std::string curUserId;
+	int size;
+
+public:
+	AccountSystem(const std::string &file);
+	~AccountSystem();
+
+private:
+	void printToBack(const std::string &str);
+	std::string read(int address, int len);
+	void write(int address, const std::string &str);
+
+private:
+	int readLevel(int address);
+	std::string readUserId(int address);
+	std::string readPassword(int address);
+
+public:
+	void add(int level, const std::string &userId,
+		const std::string &password, const std::string &name);
+	void erase(const std::string &userId);
+	void changePassword(const std::string &userId, const std::string &newPassword,
+		const std::string &oldPassword = "");
+	void login(const std::string &userId, const std::string &password = "");
+	void logout();
+};
+
+#endif Account_H
