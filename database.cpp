ddc58ea269e99120d3e86cb64a96d83e2627658b
Gabriel <zhangzhicheng1@126.com>
2018-12-02 01:03:37 +0800

    Completed, correctness not tested

--- database.cpp
+++ database.cpp
@@ -13,25 +13,35 @@ DataType::DataType(std::string str)
 	ISBN = str.substr(0, ISBNLen);
 	name = str.substr(ISBNLen, StringLen);
 	author = str.substr(ISBNLen + StringLen, StringLen);
-	price = stringToInteger(str.substr(ISBNLen + StringLen * 2, NumLen));
-	quantity = stringToInteger(str.substr(ISBNLen + StringLen * 2 + NumLen, NumLen));
+	keyword = str.substr(ISBNLen + StringLen * 2, StringLen);
+	price = stringToInteger(str.substr(ISBNLen + StringLen * 3, NumLen));
+	quantity = stringToInteger(str.substr(ISBNLen + StringLen * 3 + NumLen, NumLen));
+	deleted = str[str.length() - 1] - '0';
+}
+
+DataType::DataType(std::string _ISBN, std::string _name, std::string _author,
+	std::string _keyword, int _price, int _quantity)
+	: ISBN(_ISBN), name(_name), author(_author), keyword(_keyword), price(_price), quantity(_quantity)
+{
+	/* Empty */
 }
 
 std::string DataType::printToString()
 {
 	char s[ISBNLen + StringLen * 2 + NumLen * 2];
-	sprintf(s,"%-20s", ISBN);
+	sprintf(s, "%-20s", ISBN);
 	sprintf(s + ISBNLen, "%-40s", name);
 	sprintf(s + ISBNLen + StringLen, "%-40s", author);
-	sprintf(s + ISBNLen + 2 * StringLen, "%-5d", price);
-	sprintf(s + ISBNLen + 2 * StringLen + NumLen, "%-5d", quantity);
-	std::string ret = s;
+	sprintf(s + ISBNLen + 2 * StringLen, "%-40s", keyword);
+	sprintf(s + ISBNLen + 3 * StringLen, "%-5d", price);
+	sprintf(s + ISBNLen + 3 * StringLen + NumLen, "%-5d", quantity);
+	std::string ret = s + (char)(deleted + '0');
 	return ret;
 }
 
 Database::Database(const std::string &file, int _offset, int len) : offset(_offset), readLen(len)
 {
-	dataIO.open(file, std::ios::app|std::ios::out);
+	dataIO.open(file, std::ios::app | std::ios::out);
 	dataIO.seekg(0);
 	dataIO.read(reinterpret_cast<char*>(&startAddress), sizeof(int));
 }
@@ -59,6 +69,13 @@ std::string Database::readWholeBlock(int address)
 	std::string ret = t;
 	delete[] t;
 	return ret;
+}
+
+void Database::writeWholeBlock(int address, std::string str)
+{
+	char *s = (char*)str.c_str();
+	dataIO.seekg(address);
+	dataIO.write(s, str.length());
 }
 
 bool Database::inCurBlock(std::string &key, int curSize)
@@ -103,9 +120,49 @@ DataType Database::read(std::string &key)
 	return DataType();
 }
 
-void Database::writeInsideBlock(std::string &key, int address, int size)
+int Database::createNewBlock(int size, int next)
+{
+	dataIO.seekg(0, std::ios::end);
+	dataIO.write(reinterpret_cast<char*>(&size), sizeof(int));
+	dataIO.seekg(BlockLen - sizeof(int), std::ios::cur);
+	dataIO.write(reinterpret_cast<char*>(&next), sizeof(int));
+}
+
+int Database::split(int start, int beginEle, int size)
+{
+	int t = createNewBlock(size - beginEle + 1);
+	int t2 = t;
+
+	//link current to new block
+	dataIO.seekg(start + BlockLen);
+	dataIO.write(reinterpret_cast<char*>(&t), sizeof(t));
+
+	//resize the current block
+	dataIO.seekg(start);
+	int curSize = beginEle - 1;
+	dataIO.write(reinterpret_cast<char*>(&curSize), sizeof(curSize));
+
+	//write elements into new block
+	start = start + sizeof(int) + DataTypeLen * (size - 1);
+	t += sizeof(int);
+	for (int i = beginEle; i <= size; i++, t += DataTypeLen)
+	{
+		std::string cur = readWholeBlock(start);
+		writeWholeBlock(t, cur);
+	}
+
+	return t2;
+}
+
+void Database::cleanup()
+{
+	//TODO: Write cleanup()
+}
+
+void Database::writeInsideBlock(std::string &key, int address, int size, std::string value)
 {
 	int pre = 0;
+	int start = address;
 	for (int i = 1; i <= size; i++)
 	{
 		std::string cur = readBlock(address, readLen);
@@ -118,12 +175,21 @@ void Database::writeInsideBlock(std::string &key, int address, int size)
 			return;
 		}
 		if (cur < key) pre = address;
+		else
+		{
+			int t = split(start, address, size - i + 1);
+			int newBlock = createNewBlock(1, t);
+			//link the first block to the new block
+			dataIO.seekg(start + BlockLen - sizeof(int));
+			dataIO.write(reinterpret_cast<char*>(&newBlock), sizeof(int));
+			//write data to the new block
+			writeWholeBlock(newBlock, value);
+		}
 		address += DataTypeLen;
 	}
-	createNewNode()
 }
 
-void Database::write(std::string &key, DataType &data)
+void Database::write(std::string key, DataType &data)
 {
 	int curAddress = startAddress;
 	while (true)
@@ -132,7 +198,7 @@ void Database::write(std::string &key, DataType &data)
 		int curSize;
 		dataIO.read(reinterpret_cast<char*>(&curSize), sizeof(curSize));
 
-		if (inCurBlock(key, curSize)) writeInsideBlock(key, dataIO.tellg(), curSize);
+		if (inCurBlock(key, curSize)) writeInsideBlock(key, dataIO.tellg(), curSize, data.printToString());
 		else
 		{
 			curAddress += BlockLen - sizeof(int);
@@ -143,7 +209,17 @@ void Database::write(std::string &key, DataType &data)
 			curAddress = next;
 		}
 	}
-	//new block at the end
-	dataIO.seekg(curAddress);
-	createNewNode(curAddress, -1, key, data);
+	//craete new block at the end
+	int t = createNewBlock(1, -1);
+	dataIO.seekg(curAddress + DataTypeLen - sizeof(int));
+	dataIO.write(reinterpret_cast<char*>(&t), sizeof(t));
+	//write data to the new block
+	writeWholeBlock(t + sizeof(int), data.printToString());
+}
+
+void Database::erase(std::string key)
+{
+	auto data = read(key);
+	data.deleted = true;
+	write(key, data);
 }
--- database.h
+++ database.h
@@ -9,7 +9,7 @@
 static const int ISBNLen = 20;
 static const int StringLen = 40;
 static const int NumLen = 5;
-static const int DataTypeLen = ISBNLen + StringLen * 2 + NumLen * 2;
+static const int DataTypeLen = ISBNLen + StringLen * 3 + NumLen * 2 + 1;
 
 static const int BlockSize = 100;
 static const int BlockLen = sizeof(int) * 2 + BlockSize * DataTypeLen;
@@ -18,14 +18,18 @@ int stringToInteger(const std::string &str);
 
 class DataType
 {
-private:
+public:
 	std::string ISBN; //aligned to 20 characters
-	std::string name, author; //aligned to 40 characters(20 chinese characters)
+	std::string name, author, keyword; //aligned to 40 characters(20 chinese characters)
 	int price, quantity; //aligned to 5 characters
+public:
+	bool deleted = false;
 
 public:
 	DataType() = default;
 	DataType(std::string str);
+	DataType(std::string _ISBN, std::string _name, std::string _author,
+		std::string _keyword, int _price, int _quantity);
 
 public:
 	std::string printToString();
@@ -45,18 +49,20 @@ public:
 private:
 	std::string readBlock(int offset, int len = BlockLen);
 	std::string readWholeBlock(int address);
+	void writeWholeBlock(int address, std::string str);
 	bool inCurBlock(std::string &key, int curSize);
-	
+
 	DataType readInsideBlock(std::string &key, int address, int size);
-	void writeInsideBlock(std::string &key, int address, int size);
-	void split(int address);
-	void createNewNode(int pre, int next, std::string &key);
+	void writeInsideBlock(std::string &key, int address, int size, std::string value = "");
+	int split(int start, int beginEle, int size);
+	int createNewBlock(int size = 0, int next = -1); //create a new block without filling the elements
 	void cleanup();
 
 public:
 	DataType read(std::string &key);
-	void write(std::string &key, DataType &data);
-	void erase(int address);
+	void write(std::string key, DataType &data);
+	void erase(std::string key);
+};
 
 #endif Database_H
 
